ec5b426bda6e34811c77811498365dc7981b7fde
nickolasbradham <nickolasbradham@gmail.com>
2025-02-01 12:55:44 -0700

    Unsimplify.

--- src/nbradham/alphaGen/Manager.java
+++ src/nbradham/alphaGen/Manager.java
@@ -62,7 +62,7 @@ public final class Manager {
 		}
 	};
 
-	private final int total;
+	private final int radius, total;
 
 	private Dir dir = Dir.DOWN;
 	private int x = -1, y = 0, count = 0;
@@ -71,6 +71,7 @@ public final class Manager {
 
 	public Manager(short chunkRadius) {
 		int t = Math.ceilDiv(chunkRadius, 10);
+		radius = t / 2;
 		total = ++t * t;
 	}
 
@@ -103,9 +104,9 @@ public final class Manager {
 			if (incMax = !incMax)
 				++lineMax;
 		}
-		if (count < total) {
+		if (Math.abs(x += dir.dx) <= radius || Math.abs(y += dir.dy) <= radius) {
 			System.out.printf("Area %d out of %d requested.%n", ++count, total);
-			return new int[] { x += dir.dx, y += dir.dy };
+			return new int[] { x, y };
 		}
 		return null;
 	}
