6be831ca47f234ddfd2923b3cc15ee0bc26492fb
nickolasbradham <nickolasbradham@gmail.com>
2025-02-01 12:47:56 -0700

    Logic fix.

--- src/nbradham/alphaGen/Manager.java
+++ src/nbradham/alphaGen/Manager.java
@@ -104,7 +104,7 @@ public final class Manager {
 			if (incMax = !incMax)
 				++lineMax;
 		}
-		if (Math.abs(x += dir.dx) <= radius || Math.abs(y += dir.dy) <= radius) {
+		if (Math.abs(x += dir.dx) < radius || Math.abs(y += dir.dy) < radius) {
 			System.out.printf("Area %d out of %d requested.%n", ++count, total);
 			return new int[] { x, y };
 		}
